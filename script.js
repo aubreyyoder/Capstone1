b1bdbfc7dfca654652441b913dcca9d4e48a7d79
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-02-19 23:15:42 -0800

    Updated input page as well as currency symbol pages

--- script.js
+++ script.js
@@ -31,50 +31,92 @@ function hamburgerDropDown() {
 function attachCurrencyToSymbol() {
 
 }
+
+// Create function that attaches currency button to value
+
+
 // Create function to display select-currency page
 function displayCurrencySelections() {
-    $('.currency-symbols').append(
-        `<li class="currency"><a href="USD">USD SYMBOL</a></li>
-        <li class="currency"><a href="EUR">EUR SYMBOL</a></li>
-        <li class="currency"><a href="GBD">GBD SYMBOL</a></li>
-        <li class="currency"><a href="CAD">CAD SYMBOL</a></li>
-        <li class="currency"><a href="CHF">CHF SYMBOL</a></li>
-        <li class="currency"><a href="SEK">SEK SYMBOL</a></li>
-        <li class="currency"><a href="MXN">MXN SYMBOL</a></li>
-        <li class="currency"><a href="AUD">AUD SYMBOL</a></li>`
+    $('.old-currencies').append(
+        `<li class="currency"><button id="EUR" value="EURO">€ (EUR)</button></li>
+        <li class="currency"><button id="JAP" value="JAP">¥ (JAP)</button></li>
+        <li class="currency"><button id="GBD" value="GBD">£ (GBD)</button></li>
+        <li class="currency"><button id="CAD" value="CAD">$ (CAN)</button></li>
+        <li class="currency"><button id="USD" value="USD">$ (USD)</button></li>
+        <li class="currency"><button id="NZD" value="NZD">$ (NZD)</button></li>
+        <li class="currency"><button id="MXN" value="MXN">$ (MEX)</button></li>
+        <li class="currency"><button id="AUD" value="AUD">$ (AUD)</button></li>`
     );
     $('.old-currencies').removeClass('hidden');
-}
-
-// Create function to append currency symbols to '#select-currency' page
-function currencySymbols() {
-
+    $('button').on('click', event => {
+        const oldCurrency = $(this).value;
+        console.log(oldCurrency);
+        //$('.old-currencies').addClass('hidden');
+        displayInputPage(oldCurrency);
+    });
 }
 
 // Create function to display number input page with selected currency symbol
-function displayInputPage() {
-
+function displayInputPage(oldCurrency) {
+    const exchangeAmount = $('#js-amount-input').val();
+    
+    $('#input-currency-symbol').append(
+        `<li id="input-symbol">${oldCurrency}</li>`
+    );
+    $('.amount').removeClass('hidden');
+    $('#submit').on('click', event => {
+        displayDesiredCurrencySelection();
+    })
 }
 
 // Create function to display desired currency page
 function displayDesiredCurrencySelection() {
-    $('.currency-symbols').append(
-        `<li><a href="USD">USD SYMBOL</a></li>
-        <li><a href="EURO">EUR SYMBOL</a></li>
-        <li><a href="GBD">GBD SYMBOL</a></li>
-        <li><a href="CAD">CAD SYMBOL</a></li>
-        <li><a href="CHF">CHF SYMBOL</a></li>
-        <li><a href="SEK">SEK SYMBOL</a></li>
-        <li><a href="MXN">MXN SYMBOL</a></li>
-        <li><a href="AUD">AUD SYMBOL</a></li>`
-    );
     $('.new-currencies').removeClass('hidden');
-
+    $('.new-currencies').append(
+        `<li class="currency"><button id="EUR" value="EURO">€ (EUR)</button></li>
+        <li class="currency"><button id="JAP" value="JAP">¥ (JAP)</button></li>
+        <li class="currency"><button id="GBD" value="GBD">£ (GBD)</button></li>
+        <li class="currency"><button id="CAD" value="CAD">$ (CAN)</button></li>
+        <li class="currency"><button id="USD" value="USD">$ (USD)</button></li>
+        <li class="currency"><button id="NZD" value="NZD">$ (NZD)</button></li>
+        <li class="currency"><button id="MXN" value="MXN">$ (MEX)</button></li>
+        <li class="currency"><button id="AUD" value="AUD">$ (AUD)</button></li>`
+    );
+    $('.amount').addClass('hidden');
+    $('.old-currencies').addClass('hidden');
+    
+    $('button').on('click', event => {
+        const newCurrency = $(this).value;
+        console.log(newCurrency);
+        $('.new-currencies').addClass('hidden');
+        displayConfirmation(oldCurrency, exchangeAmount, newCurrency);
+    });
 }
 
 // Create function to show "is this correct? Page"
-function displayConfirmation() {
+function displayConfirmation(oldCurrency, exchangeAmount, newCurrency) {
+    $('.confirmation').removeClass('hidden');
+    $('#confirmation-paragraph').append(
+        `'Just to confirm:
+        You'd like to convert '${oldCurrency} ${exchangeAmount}' to '${newCurrency}'.
+        Correct?'
+        <button value="yes">YES</button>
+        <button value="no">NO</button>`
+    );
+    $('button').on('click', event => {
+        if ($(this).value == yes) {
+            displayResults();
+        } else if ($(this).value == no) {
+            alert(`YOU MUST CLICK 'YES'!!`);
+        }
+    })
+        displayResults();
+}
 
+// Creates a function that fetches API info for exchange rates
+function getExchangeRates() {
+    console.log('...Fetching exchange rates');
+    
 }
 
 // Create function to append results
@@ -95,6 +137,7 @@ function goToFavoritesPage() {
 // Create function for landing button to take to '#select-currency' page
 function watchForm() {
     // event handler
+    console.log();
     $('#landing-btn').on('click', event => {
         event.preventDefault;
         $('.landing').addClass('hidden');
